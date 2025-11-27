6aa722e860e7154435f8d1f15e124af95987699e
developer-hajun <juni8828@naver.com>
2025-11-27 15:40:30 +0900

    chroma Vector DB를 이용한 RAG를 위한 데이터 저장 알고리즘 개발

--- src/main/java/ssafy/realty/RealtyApplication.java
+++ src/main/java/ssafy/realty/RealtyApplication.java
@@ -1,13 +1,15 @@
 package ssafy.realty;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 특정 자동 설정 클래스를 제외(exclude) 시킵니다.
 @SpringBootApplication
+@MapperScan(basePackages = "ssafy.realty.Mapper")
 public class RealtyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RealtyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RealtyApplication.class, args);
+    }
 }
