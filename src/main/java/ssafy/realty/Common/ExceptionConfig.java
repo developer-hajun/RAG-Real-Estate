ac3401422ec46a7d23df1b5e5eceb4297a5f2d85
developer-hajun <juni8828@naver.com>
2025-11-28 17:08:57 +0900

    UserMapper.XML

---
+++ src/main/java/ssafy/realty/Common/ExceptionConfig.java
@@ -0,0 +1,10 @@
+package ssafy.realty.Common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class ExceptionConfig extends WebMvcConfigurationSupport {
+
+
+}
