719672f417add5693651af37b9744fd78d2e9838
kbi0418 <kbi0418@naver.com>
2025-11-28 17:07:50 +0900

    User 관련 기능 Mapper 및 XML 작성

--- src/main/java/ssafy/realty/Mapper/UserMapper.java
+++ src/main/java/ssafy/realty/Mapper/UserMapper.java
@@ -1,8 +1,26 @@
 package ssafy.realty.Mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Service;
+import ssafy.realty.Entity.SearchHistory;
+import ssafy.realty.Entity.User;
+
+import java.util.List;
 
 @Mapper
 public interface UserMapper {
+    //회원가입
+    void saveUser(User user);
+
+    // 이메일 중복 체크 및 로그인용 조회
+    User findByEmail(String email);
+
+    // ID로 회원 조회 (프로필)
+    User findById(int id);
+
+    // 회원 정보 수정
+    int update(User user);
+
+    // 검색 기록 조회
+    List<SearchHistory> findSearchHistoryByUserId(int userId);
+
 }
