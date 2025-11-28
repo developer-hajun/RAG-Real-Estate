e8d2fdfeb071984e9cf6a9d8fc8c62eca1176a16
developer-hajun <juni8828@naver.com>
2025-11-28 10:00:49 +0900

    댓글 관련 SQL 기본 로직 추가

--- src/main/java/ssafy/realty/Entity/Comment.java
+++ src/main/java/ssafy/realty/Entity/Comment.java
@@ -1,5 +1,6 @@
 package ssafy.realty.Entity;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +14,18 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Comment {
+
     private int id;
     private Post post;
     private User user;
     private String content;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
+
+    @Nullable
     private Comment parentComment;
+
     private List<Comment> replies;
+
 
 }
--- src/main/java/ssafy/realty/Mapper/CommentMapper.java
+++ src/main/java/ssafy/realty/Mapper/CommentMapper.java
@@ -1,7 +1,22 @@
 package ssafy.realty.Mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import ssafy.realty.Entity.Comment;
+
+import java.util.List;
 
 @Mapper
 public interface CommentMapper {
+
+    List<Comment> selectCommentsByUserId(Integer userId);
+
+    List<Comment> selectPostByID(Integer postId);
+
+    int updateComment(Comment comment);
+
+    int insertComment(Comment comment);
+
+    int deleteComment(Integer commentId);
+
+
 }
